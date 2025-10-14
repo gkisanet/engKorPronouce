5ec2ccfe6a1f4a1c5d6a5d01e3dbfea48e94728a
gkisanet <gkisanet@gmail.com>
2025-10-14 16:37:17 +0900

    add tailwind style v3

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import QuizPage from './pages/QuizPage'
 
 export default function App() {
   return (
-    <main className="min-h-screen w-full flex flex-col items-center justify-center bg-background p-4">
+    <main className="min-h-screen w-full flex flex-col items-center justify-center bg-blue-500 p-4">
       <QuizPage />
     </main>
   )
